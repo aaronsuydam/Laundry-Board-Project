1fac96cfc4b32484407869d2db0d7d5e7e4b49dc
atxfjrotc <73612843+atxfjrotc@users.noreply.github.com>
2022-04-20 15:24:32 -0400

    First Draft of Map Implementation

--- src/laundryBackend.cpp
+++ src/laundryBackend.cpp
@@ -24,30 +24,10 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include "Sessions.cpp"
 using namespace std;
 
 
-class Session
-{
-    public:
-    string user;
-    int machinesUsed[2];
-    int times[2];
-
-    Session()
-    {
-        user = "";
-    }
-
-    Session(string user, int washerUsed, int dryerUsed, int washTime, int dryTime)
-    {
-        this->user = user;
-        machinesUsed[1] = washerUsed;
-        machinesUsed[2] = dryerUsed;
-        times[1] = washTime;
-        times[2] = dryTime;
-    }
-};
 
 class AVLTree
 {
